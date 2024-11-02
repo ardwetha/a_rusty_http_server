8f69d906a242c5eef194c7f4c5f9fda1275ee171
Zacuue <boeckchensemail@gmx.de>
2024-11-02 03:04:21 +0100

    Shit works now

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+extern crate core;
+
 mod response;
+mod parser;
+
 use std::io::{self, Write};
 use std::io::{BufRead, BufReader};
 use std::net::{Shutdown, TcpListener, TcpStream};
@@ -7,7 +11,7 @@ use std::str::FromStr;
 fn handle_connection(mut stream: TcpStream) -> io::Result<()> {
     println!("New Connection");
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
+    let http_request: Vec<String> = buf_reader
         //Reads the Buffer Line by line
         .lines()
         //Converts from iterator one into iterator two (Unwraps result)
@@ -16,18 +20,19 @@ fn handle_connection(mut stream: TcpStream) -> io::Result<()> {
         .take_while(|line| !line.is_empty())
         //Transform into a vector of type string
         .collect();
-    let mut response = response::Response::new();
-    response.body = String::from_str("<h1>Hello World</h1>").unwrap();
-    for line in http_request {
-        println!("{}", line);
+
+    for line in http_request.to_owned() {
+        println!("{} END", line);
     }
-    stream.write(response.convert_to_string().as_bytes())?;
+    let (header, body) = parser::generate_response(http_request);
+    stream.write(header.as_bytes())?;
+    stream.write(body.as_slice())?;
     stream.shutdown(Shutdown::Both)
 }
 
 fn main() -> std::io::Result<()> {
     println!("Server Starting Up");
-    let listener = TcpListener::bind("127.0.0.1:80").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
     for stream in listener.incoming() {
         handle_connection(stream?)?;
--- src/response/mod.rs
+++ src/response/mod.rs
@@ -1,27 +1,28 @@
 use std::str::FromStr;
 
 mod enum_options;
+
 pub struct Response {
     pub status_code: String,
     pub content_type: String,
     pub connection: enum_options::ConnectionOptions,
-    pub body: String,
+    pub body: Vec<u8>,
 }
 
 impl Response {
     pub fn new() -> Self {
         //Just some default values
         Response {
-            status_code: String::from_str("HTTP/1.1 200 OK").unwrap(),
+            status_code: String::from_str("200 OK").unwrap(),
             content_type: String::from_str("text/html; charset=utf-8").unwrap(),
             connection: enum_options::ConnectionOptions::Close,
-            body: String::from_str("").unwrap(),
+            body: Vec::new(),
         }
     }
 
-    //Yes this is basically like a deconstructor, because the function takes ownership
-    pub fn convert_to_string(self) -> String {
+    pub fn convert_to_string(&self) -> String {
         let mut response = String::new();
+        response.push_str("HTTP/1.1 ");
         response.push_str(&self.status_code);
         response.push_str("\r\n");
         response.push_str("Content-Type: ");
@@ -38,7 +39,6 @@ impl Response {
             }
         }
         response.push_str("\r\n");
-        response.push_str(&self.body);
         response
     }
 }
